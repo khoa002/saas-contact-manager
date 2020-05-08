c13e4bb6c72ecd7f437fcb51b3139daa52794be6
Khoa Nguyễn <khoa002@gmail.com>
2020-05-08 11:09:18 -0700

    Add button click tracking

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,6 +29,17 @@ class HomeController extends Controller
 
     public function trackClick()
     {
-        return 'test';
+        if (env('KLAVIYO_SYNC_ENABLED') == true && !empty(env('KLAVIYO_API_TOKEN'))) {
+            $user = Auth::user();
+            $service = new \Klaviyo(env('KLAVIYO_API_TOKEN'));
+            $service->track(
+                'Button clicked',
+                ['$email' => $user->email],
+                [],
+                now()
+            );
+            return 'You clicked a button!';
+        }
+        return "You clicked a button! But tracking isn't set up.";
     }
 }
